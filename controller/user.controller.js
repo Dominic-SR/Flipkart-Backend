048afd68a2e0d81be92adcc33239f1288a9958f3
Dominic-SR <dominicseduraja3@gmail.com>
2025-10-29 21:39:15 +0530

    create controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,1 +1,15 @@
 import userModal from '../module/userModal'
+
+export const createUser = async (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+    return res.status(400).json({ message: 'Name and Email are required' });
+  }
+
+  try {
+    await addUser(name, email);
+    res.status(201).json({ message: 'User added successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding user', error });
+  }
+}
