6c63f5fb821ed38453b59323a34d0a2b14416615
Dominic-SR <dominicseduraja3@gmail.com>
2025-10-31 10:12:45 +0530

    sequalize create db setup

--- config/config.cjs
+++ config/config.cjs
@@ -6,6 +6,13 @@ module.exports = {
     password: process.env.DB_PASS || "",
     database: process.env.DB_NAME || "flipkart",
     host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+    production: {
+    username: "root",
+    password: "",
+    database: "testdb_prod",
+    host: "localhost",
     dialect: "mysql"
   }
 };
